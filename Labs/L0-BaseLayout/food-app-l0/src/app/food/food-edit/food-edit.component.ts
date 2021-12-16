5dd8148459ed4833dca584081aa5e9b5fe702a78
David <david.reitgruber@gmail.com>
2021-12-16 13:20:27 +0100

    Lab 5 done

--- Labs/L0-BaseLayout/food-app-l0/src/app/food/food-edit/food-edit.component.ts
+++ Labs/L0-BaseLayout/food-app-l0/src/app/food/food-edit/food-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FoodItem } from '../foodItem';
 
 @Component({
@@ -11,13 +12,26 @@ export class FoodEditComponent implements OnInit {
   @Input() food : FoodItem;
   @Output() saveFood: EventEmitter<FoodItem> = new EventEmitter();
 
-  constructor() { }
+  foodForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+  }
 
   ngOnInit(): void {
+    this.foodForm = this.fb.group({
+      id: [this.food.id],
+      name: [this.food.name, [Validators.required, Validators.minLength(3)]],
+      price: [this.food.price],
+      calories: [this.food.calories]
+    });
   }
 
   doSave(){
     this.saveFood.emit(this.food);
+  }
+
+  saveForm(form: any){
+    this.saveFood.emit(form.value);
   }
 
 
